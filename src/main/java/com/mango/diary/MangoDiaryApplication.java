fb7615231410eadf07795126ea0782c1a60eb3eb
Dohun Kim <dohun8758@naver.com>
2024-07-19 19:12:38 +0900

    init

---
+++ src/main/java/com/mango/diary/MangoDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mango.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoDiaryApplication.class, args);
+    }
+
+}
