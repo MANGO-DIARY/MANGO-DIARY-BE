cf7cbc0dbd14dc9c486fe115508091b16fa8a7ee
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-07-27 04:08:22 +0900

    [feat] : enum emotion 변경
    - 영문 감정 -> 한국어로 변경
      이후 아래 있던 코드 불필요해 삭제

--- src/main/java/com/mango/diary/common/enums/Emotion.java
+++ src/main/java/com/mango/diary/common/enums/Emotion.java
@@ -4,33 +4,17 @@ package com.mango.diary.common.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Getter
 public enum Emotion {
-    HAPPY("기쁨"),
-    FUN("신남"),
-    PEACEFUL("평온"),
-    LOVE("설렘"),
-    SAD("슬픔"),
-    ANGRY("분노"),
-    FEAR("두려움"),
-    LONELINESS("외로움");
-
-    private final String emotion;
-
-    private static final Map<String, Emotion> BY_EMOTION = new HashMap<>();
-
-    static {
-        for (Emotion e : values()) {
-            BY_EMOTION.put(e.emotion, e);
-        }
-    }
-
-    public String changeString () {
-        return emotion;
-    }
+    기쁨,
+    신남,
+    우울,
+    불안,
+    슬픔,
+    행복,
+    평온,
+    분노,
 }
 
