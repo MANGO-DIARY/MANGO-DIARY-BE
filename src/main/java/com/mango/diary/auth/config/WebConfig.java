790f4c5aaf92afecee2cc7b3ba84aaa4200409d9
Dohun Kim <dohun8758@naver.com>
2024-07-28 14:01:22 +0900

    [feat]: WebConfig Configuration 어노테이션 추가

--- src/main/java/com/mango/diary/auth/config/WebConfig.java
+++ src/main/java/com/mango/diary/auth/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.mango.diary.auth.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
